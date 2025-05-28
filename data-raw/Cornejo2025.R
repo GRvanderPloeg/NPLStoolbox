45538404fac5e2d9fedd71f716416c9f8632cc26
GRvanderPloeg <g.r.ploeg@uva.nl>
2025-05-28 16:06:13 +0200

    Update to Cornejo2025
    pH added to salivary biochemistry tensor
    blood hormone levels are now a tensor
    clinical measurements are now a tensor
    subject metadata is a separate matrix

--- data-raw/Cornejo2025.R
+++ data-raw/Cornejo2025.R
@@ -79,7 +79,7 @@ cytokine$mode2 = cytokine$mode2 %>% select(-index)
 cytokine$mode3 = cytokine$mode3 %>% mutate(newTimepoint=timepointMetadata) %>% select(-timepointMetadata,-index)
 
 # Salivary biochemistry
-biochemistry = saliva_sampleMeta %>% select(-value, -BarcodeSequence, -LinkerPrimerSequence, -Niche, -Timepoint, -Age, -qPCR_16S_ng_ul, -FQ_ng_ul, -Participant_code, -Testosterone_nmol_L, -Free_testosterone_Vermeulen_pmol_L, -Estradiol_pmol_ml, -Description,-GenderID, -LH_U_L, -SHBG_nmol_L)
+biochemistry = saliva_sampleMeta %>% left_join(otherMeta %>% select(subject,newTimepoint,pH)) %>% select(-value, -BarcodeSequence, -LinkerPrimerSequence, -Niche, -Timepoint, -Age, -qPCR_16S_ng_ul, -FQ_ng_ul, -Participant_code, -Testosterone_nmol_L, -Free_testosterone_Vermeulen_pmol_L, -Estradiol_pmol_ml, -Description,-GenderID, -LH_U_L, -SHBG_nmol_L)
 Xlong = biochemistry %>% select(-subject,-newTimepoint) %>% as.matrix()
 biochemistry = parafac4microbiome::reshapeData(Xlong, biochemistry$subject, colnames(Xlong), biochemistry$newTimepoint)
 
@@ -90,10 +90,48 @@ biochemistry$mode2 = biochemistry$mode2 %>% select(-index)
 biochemistry$mode3 = biochemistry$mode3 %>% mutate(newTimepoint=timepointMetadata) %>% select(-timepointMetadata,-index)
 
 # Blood hormone levels
-blood_hormones = saliva_sampleMeta %>% select(subject, GenderID, newTimepoint, Free_testosterone_Vermeulen_pmol_L, Estradiol_pmol_ml, LH_U_L, SHBG_nmol_L)
+df = saliva_sampleMeta %>% filter(newTimepoint != 6) %>% select(subject, newTimepoint, Free_testosterone_Vermeulen_pmol_L, Estradiol_pmol_ml, LH_U_L, SHBG_nmol_L)
+blood_hormones = parafac4microbiome::reshapeData(df %>% select(-subject,-newTimepoint),
+                                                 subjectMetadata = df$subject,
+                                                 timepointMetadata = df$newTimepoint,
+                                                 featureMetadata = c("Free_testosterone_Vermeulen_pmol_L", "Estradiol_pmol_ml", "LH_U_L", "SHBG_nmol_L"))
+colnames(blood_hormones$mode1) = c("subject", "index")
+blood_hormones$mode1 = blood_hormones$mode1 %>% left_join(saliva_sampleMeta %>% mutate(subject=as.character(subject)) %>% select(subject,GenderID) %>% unique()) %>% select(-index)
+blood_hormones$mode2 = blood_hormones$mode2 %>% select(-index)
+blood_hormones$mode3 = blood_hormones$mode3 %>% mutate(newTimepoint=timepointMetadata) %>% select(-timepointMetadata,-index)
 
 # Clinical measurements
-clinical = otherMeta %>% left_join(saliva_sampleMeta %>% filter(newTimepoint==12) %>% select(subject, Age))
+df = read.csv("./data-raw/Cornejo2025/CODS_XI.csv")
+df = df[1:40,1:10] %>% as_tibble() %>% filter(Participant_code %in% saliva_sampleMeta$Participant_code)
+
+CODS = df %>% select(Participant_code, CODS_BASELINE, CODS_3_MONTHS, CODS_6_MONTHS, CODS_12_MONTHS) %>% pivot_longer(-Participant_code) %>% mutate(timepoint = NA)
+CODS[CODS$name == "CODS_BASELINE", "timepoint"] = 0
+CODS[CODS$name == "CODS_3_MONTHS", "timepoint"] = 3
+CODS[CODS$name == "CODS_6_MONTHS", "timepoint"] = 6
+CODS[CODS$name == "CODS_12_MONTHS", "timepoint"] = 12
+CODS[CODS$value == -99, "value"] = NA
+CODS = CODS %>% mutate(subject = Participant_code, newTimepoint = timepoint, CODS = value) %>% select(-Participant_code, -name, -value, -timepoint)
+
+XI = df %>% select(Participant_code, XI_BASELINE, XI_3_MONTHS, XI_6_MONTHS, XI_12_MONTHS) %>% pivot_longer(-Participant_code) %>% mutate(timepoint = NA)
+XI[XI$name == "XI_BASELINE", "timepoint"] = 0
+XI[XI$name == "XI_3_MONTHS", "timepoint"] = 3
+XI[XI$name == "XI_6_MONTHS", "timepoint"] = 6
+XI[XI$name == "XI_12_MONTHS", "timepoint"] = 12
+XI[XI$value == -99, "value"] = NA
+XI = XI %>% mutate(subject = Participant_code, newTimepoint = timepoint, XI = value) %>% select(-Participant_code, -name, -value, -timepoint)
+
+df = otherMeta %>% left_join(CODS) %>% left_join(XI) %>% select(subject, newTimepoint, boppercent, CODS, XI) %>% filter(newTimepoint != "NA")
+clinical = parafac4microbiome::reshapeData(df %>% select(-subject,-newTimepoint),
+                                           subjectMeta=df$subject,
+                                           featureMetadata = c("BOP", "CODS", "XI"),
+                                           timepointMetadata = df$newTimepoint)
+colnames(clinical$mode1) = c("subject", "index")
+clinical$mode1 = clinical$mode1 %>% left_join(saliva_sampleMeta %>% mutate(subject=as.character(subject)) %>% select(subject,GenderID) %>% unique()) %>% select(-index)
+clinical$mode2 = clinical$mode2 %>% select(-index)
+clinical$mode3 = clinical$mode3 %>% mutate(newTimepoint=timepointMetadata) %>% select(-timepointMetadata,-index)
+
+# Subject metadata
+metadata = saliva_sampleMeta %>% select(subject, GenderID, newTimepoint, Age) %>% left_join(otherMeta) %>% select(subject, GenderID, Age) %>% unique()
 
 # Store and save
 Cornejo2025 = list()
@@ -103,4 +141,5 @@ Cornejo2025$Salivary_biochemistry = biochemistry
 Cornejo2025$Salivary_cytokines = cytokine
 Cornejo2025$Blood_hormones = blood_hormones
 Cornejo2025$Clinical_measurements = clinical
+Cornejo2025$Subject_metadata = metadata
 usethis::use_data(Cornejo2025, overwrite = TRUE)
